10f6d16c4e864fbdcdcd745e84b3b195b0515906
Syedatheruddin <39413237+Syedatheruddin@users.noreply.github.com>
2019-02-22 20:09:15 +0530

    ather Changes of Error Loging

---
+++ IMS/ErrorPages/DefaultError.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IMS.ErrorPages
+{
+    public partial class DefaultError : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Session["ErrorId"]!= null)
+            {
+                lblErrorID.InnerText = Session["ErrorId"].ToString();
+            }
+        }
+    }
+}
