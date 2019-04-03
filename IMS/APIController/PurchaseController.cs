b5d10de3258cfb70428375ce3ad8b2560d8622fe
shakeeb <contacttoshakeeb@gmail.com>
2019-04-03 20:23:42 +0530

    Api testing Changes

---
+++ IMS/APIController/PurchaseController.cs
@@ -0,0 +1,57 @@
+﻿using IMSBLL.DTO;
+using IMSBLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IMS.APIController
+{
+    
+    /// <summary>
+    /// Purchase Controller 
+    /// </summary>
+    //[RoutePrefix("Purchase/Api")]
+    public class PurchaseController : ApiController
+    {
+        /// <summary>
+        /// Purchase save functionality
+        /// </summary>
+        /// <param name="purchaseViewModel"></param>
+        /// <returns></returns>
+        //[Route("SavePurchase")]
+        [HttpPost]
+        public IHttpActionResult SavePurchase(PurchaseViewModel purchaseViewModel)
+        {
+            PurchaseService purchaseService = new PurchaseService();
+            var purchase = purchaseService.SavePurchase(purchaseViewModel);
+            if (purchase == null)
+            {
+                return BadRequest("Error has occured.");
+            }
+
+            return Ok(purchase);
+        }
+
+        /// <summary>
+        /// Purchase save functionality
+        /// </summary>
+        /// <returns></returns>
+       // [Route("GetPurchase")]
+        [HttpGet]
+        public IHttpActionResult GetPurchase()
+        {
+            PurchaseService purchaseService = new PurchaseService();
+            var purchase = purchaseService.GetPurchases();
+            if (purchase == null)
+            {
+                return BadRequest("Error has occured.");
+            }
+
+            return Ok(purchase);
+        }
+    }
+
+}
